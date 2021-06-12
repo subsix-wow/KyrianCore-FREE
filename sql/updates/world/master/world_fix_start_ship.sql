3009a49f511911158d36962226b06fcca14499b5
welder1976 <zemetskia@gmail.com>
2021-06-12 12:27:37 +0300

    fix start ship

---
+++ sql/updates/world/master/world_fix_start_ship.sql
@@ -0,0 +1,7 @@
+DELETE FROM `gameobject_template_addon` WHERE `entry`=349879;
+INSERT INTO `gameobject_template_addon` (`entry`, `faction`, `flags`, `mingold`, `maxgold`, `WorldEffectID`, `AIAnimKitID`) VALUES
+(349879, 0, 1048616, 0, 0, 0, 0);
+
+DELETE FROM `gameobject_template_addon` WHERE `entry`=333479;
+INSERT INTO `gameobject_template_addon` (`entry`, `faction`, `flags`, `mingold`, `maxgold`, `WorldEffectID`, `AIAnimKitID`) VALUES
+(333479, 0, 1048616, 0, 0, 0, 0); 
