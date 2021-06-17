8c47dcac65944d06eb399e7cf9d98b10dca7b73e
subsix-wow <attentat22@gmail.com>
2021-06-17 23:31:23 +0200

    Begin Spell Proc Work

---
+++ sql/updates/world/master/2021_06_17_00_world.sql
@@ -0,0 +1,6 @@
+-- Mage Arcane Charge Proc
+REPLACE INTO `spell_proc` VALUES (36032, 64, 3, 0, 0, 0, 0, 0, 1, 1, 0, 8, 0, 0, 100, 0, 0);
+-- Shaman Windfury Weapon Proc
+REPLACE INTO `spell_proc` VALUES (33757, 8, 11, 0, 0, 256, 0, 4, 0, 0, 0, 0, 0, 0, 100, 0, 0);
+-- Monk Lifecycles Proc
+REPLACE INTO `spell_proc` VALUES (197915, 1, 53, 0, 0, 0, 0, 16384, 2, 1, 0, 0, 0, 0, 100, 0, 0);
